36b6812b607b30d399e86d03d7137a66edd52f3a
kashifimran <kashifimran2008@gmail.com>
2020-08-05 16:53:30 +0500

    a few changes in settings

--- static/src/scripts/app/account/pages/settings.js
+++ static/src/scripts/app/account/pages/settings.js
@@ -1,11 +1,13 @@
 import setjs from '@stateempire/setjs';
 import {api} from 'core/api-helper.js';
+import {cascadeRoleFlag} from 'setbp/kernel/roles.js';
 import {formErrors} from 'helpers/site-helpers.js';
 
 export default {
   templates: ['account'],
+  role: cascadeRoleFlag('public'),
   getComp: function() {
-    return setjs.getComp('account/settings', null, {
+    return setjs.getComp('account/settings', {user: api.getUser()}, {
       form: function({comp, $el, error}) {
         api.updateUser({
           data: {user: $el.formJson()},
